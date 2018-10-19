487dd72214f22ffa8b5d82cc7fef0db21938bdab
Douglas <daglach7@gmail.com>
2018-10-19 06:36:27 -0700

    feat(validation) add an error handler for capturing a wrong url error
    [#161293280]

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 
 app = Flask(__name__)
+
+@app.errorhandler(404)
+def page_not_found(e):
+
+    ''' this function is for catching anerror and 
+        returning a message incase of an invalid url '''
+    response = jsonify({"message": "Please put a valid URL"})
+    response.status_code = 405
+    return response
+
+from app.views import product_view
